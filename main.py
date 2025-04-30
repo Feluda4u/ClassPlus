c03d8fd10c52f7007af7976640e255c9ae874b28
Feluda4u <100409211+Feluda4u@users.noreply.github.com>
2025-04-30 20:36:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -323,7 +323,7 @@ async def health():
 def ping_self():
     while True:
         try:
-            res = requests.get("https://amazing-margit-ayuschatterjee-94e3bcaf.koyeb.app/health")
+            res = requests.get("https://joyous-cairistiona-feluda4u-d4c2aaa6.koyeb.app/health")
             if res.status_code == 200:
                 print("✅ Self-ping succeeded")
             else:
